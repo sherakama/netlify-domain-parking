d029ed3d8fb5b48bd9d524bbc2aab5148028d095
Sherakama <sherakama@gmail.com>
2023-11-28 12:25:57 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,21 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Stanford Domain Parking</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="This domain is temporarily parked" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          This is a temporary holding ground for domains. Don't worry. It will be a real site some day.
         </p>
-        <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
